b397aa60959f8c9bb4f1862cff3385dd4bc7a1a7
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-02-23 21:24:58 +0100

    add form and two-way-databinding to component

--- src/TodoList.js
+++ src/TodoList.js
@@ -10,13 +10,36 @@ const TodoList = () => {
     const [todos, setTodos] = useState([]);
 
     useEffect(() => {
+        // if you do a state-change in a timeout the component gets re-rendered
+        // and the effect will be reexecuted => which will trigger a new state-change
+        // => to prevent this, we need to add an empty array at the end
         setTimeout(() => {
             setTodos(data);
         }, 1000);
-    });
+    }, []);
+
+
+    const [newTodo, setNewTodo] = useState('');
 
     return (
         <div>
+            <h3>Neue Todo:</h3>
+            {/* there is no automatically two-way-databinding */}
+            <input type="text" value={newTodo} onChange={(event => {
+                const value = event.currentTarget.value;
+                setNewTodo(value);
+            })}/>
+            <button onClick={() => {
+                const myBrandNewTodo = {
+                    // id: Math.max.apply(todos.map(t => t.id)) + 1,
+                    id: Math.max(...todos.map(t => t.id)) + 1,
+                    title: newTodo,
+                    done: false,
+                }
+                setTodos([...todos, myBrandNewTodo]);
+                setNewTodo('');
+            }}>Ok</button>
+
             {todos.map((todo) => {
                 return <div key={todo.id}>{todo.title}</div>
             }) }
