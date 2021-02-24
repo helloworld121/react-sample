6d93530bcd3f3b8475b4da5e5eb7e77a395d3625
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-02-24 22:01:49 +0100

    provide bootstrap template and extract Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from 'react';
-import Item from './Item';
-import TodoList from './TodoList';
+import Header from "./fragment/Header";
+import Footer from "./fragment/Footer";
 
 const App = () => {
-  // setName triggers re-rendering => like change detection => there is no other possibility
-  // useState creates a local component-state
-  const[name, setName] = useState('World');
-
-  // lifecycle-hook => compared to NgOnInit
-  // the rendering process consists out of two steps => pre-commit, commit
-  useEffect(() => {
-    setTimeout(() => {
-      setName('Peter');
-      console.log('Async done');
-    }, 1000);
-  }, []); // the array displays the dependencies
 
 
-
-  // JSX => "{}" => activates JavaScript
   // to don't blow the DOM we can use the wrapping tag "<></>"
   // "<></>" is equal to "<React.Fragment></React.Fragment>" it is an alias
   return (
       <>
-        <div>Hello {name + 'static'}</div>
-        <Item />
-        <hr/>
-        <TodoList/>
+          <Header/>
+
+          <main className="container">
+              <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                  <h1 className="display-4">Pricing</h1>
+                  <p className="lead">Quickly build an effective pricing table for your potential customers with this
+                      Bootstrap example. It’s built with default Bootstrap components and utilities with little
+                      customization.</p>
+              </div>
+
+              <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
+                  <div className="col">
+                      <div className="card mb-4 shadow-sm">
+                          <div className="card-header">
+                              <h4 className="my-0 fw-normal">Free</h4>
+                          </div>
+                          <div className="card-body">
+                              <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/
+                                  mo</small></h1>
+                              <ul className="list-unstyled mt-3 mb-4">
+                                  <li>10 users included</li>
+                                  <li>2 GB of storage</li>
+                                  <li>Email support</li>
+                                  <li>Help center access</li>
+                              </ul>
+                              <button type="button" className="w-100 btn btn-lg btn-outline-primary">Sign up for free
+                              </button>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col">
+                      <div className="card mb-4 shadow-sm">
+                          <div className="card-header">
+                              <h4 className="my-0 fw-normal">Pro</h4>
+                          </div>
+                          <div className="card-body">
+                              <h1 className="card-title pricing-card-title">$15 <small className="text-muted">/
+                                  mo</small></h1>
+                              <ul className="list-unstyled mt-3 mb-4">
+                                  <li>20 users included</li>
+                                  <li>10 GB of storage</li>
+                                  <li>Priority email support</li>
+                                  <li>Help center access</li>
+                              </ul>
+                              <button type="button" className="w-100 btn btn-lg btn-primary">Get started</button>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col">
+                      <div className="card mb-4 shadow-sm">
+                          <div className="card-header">
+                              <h4 className="my-0 fw-normal">Enterprise</h4>
+                          </div>
+                          <div className="card-body">
+                              <h1 className="card-title pricing-card-title">$29 <small className="text-muted">/
+                                  mo</small></h1>
+                              <ul className="list-unstyled mt-3 mb-4">
+                                  <li>30 users included</li>
+                                  <li>15 GB of storage</li>
+                                  <li>Phone and email support</li>
+                                  <li>Help center access</li>
+                              </ul>
+                              <button type="button" className="w-100 btn btn-lg btn-primary">Contact us</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+              <Footer/>
+          </main>
       </>
   );
 }
