3f13b975549e06f90cf87ccfab92b1f7d0a574c1
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-02-26 11:21:31 +0100

    move TodoList files to separate directory

---
+++ src/todo/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import {useState, useEffect} from 'react';
+import Item from './Item';
+import TodoList from './TodoList';
+
+const App = () => {
+    // setName triggers re-rendering => like change detection => there is no other possibility
+    // useState creates a local component-state
+    const[name, setName] = useState('World');
+
+    // lifecycle-hook => compared to NgOnInit
+    // the rendering process consists out of two steps => pre-commit, commit
+    useEffect(() => {
+        setTimeout(() => {
+            setName('Peter');
+            console.log('Async done');
+        }, 1000);
+    }, []); // the array displays the dependencies
+
+
+
+    // JSX => "{}" => activates JavaScript
+    // to don't blow the DOM we can use the wrapping tag "<></>"
+    // "<></>" is equal to "<React.Fragment></React.Fragment>" it is an alias
+    return (
+        <>
+            <div>Hello {name + 'static'}</div>
+            <Item />
+            <hr/>
+            <TodoList/>
+        </>
+    );
+}
+
+export default App;
